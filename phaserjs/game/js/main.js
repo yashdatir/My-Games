cf2fb462c34039109743852124cee9ad8a142f50
yashdatir <yashdatir@gmail.com>
2020-08-08 16:46:29 +0530

    added functionality

--- phaserjs/game/js/main.js
+++ phaserjs/game/js/main.js
@@ -1,4 +1,5 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var platforms, player;
 
 function preload (){
     game.load.image('sky', 'assets/sky.png');
@@ -6,7 +7,7 @@ function preload (){
     game.load.image('star', 'assets/star.png');
     game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 }
-var platforms, player;
+
 function create (){
     game.physics.startSystem(Phaser.Physics.ARCADE);
     game.add.sprite(0, 0, 'sky');
@@ -27,29 +28,42 @@ function create (){
     player.body.collideWorldBounds = true;
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
-
-    cursors = game.input.keyboard.createCursorKeys()
+    
+    cursors = game.input.keyboard.createCursorKeys();
+    
+    stars = game.add.group();
+    stars.enableBody = true;
+    for (var i = 0; i < 12; i++)
+    {
+        var star = stars.create(i * 70, 0, 'star');
+        star.body.gravity.y = 200;
+        star.body.bounce.y = 0.7 + Math.random() * 0.2;
+    }
 }
 function update(){
     var hitPlatform = game.physics.arcade.collide(player, platforms);
     player.body.velocity.x = 0;
-    if (cursors.left.isDown)
-    {
+    if (cursors.left.isDown){
         player.body.velocity.x = -150;
         player.animations.play('left');
     }
-    else if (cursors.right.isDown)
-    {
+    else if (cursors.right.isDown){
         player.body.velocity.x = 150;
         player.animations.play('right');
     }
-    else
-    {
+    else{
         player.animations.stop();
         player.frame = 4;
     }
-    if (cursors.up.isDown && player.body.touching.down && hitPlatform)
-    {
+    if (cursors.up.isDown && player.body.touching.down && hitPlatform){
         player.body.velocity.y = -350;
     }
+
+    game.physics.arcade.collide(stars, platforms);
+
+    game.physics.arcade.overlap(player, stars, collectStar, null, this);
+}
+
+function collectStar (player, star) {
+    star.kill();
 }
